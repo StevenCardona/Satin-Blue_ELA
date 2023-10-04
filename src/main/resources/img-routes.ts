4174d91a0536ce7a3c1f675554b2eabf4085cc3f
Steven Cardona <steven.cardona@platec.co>
2023-10-04 14:55:52 -0500

    Fix routes for the images and the homepage

--- src/main/resources/img-routes.ts
+++ src/main/resources/img-routes.ts
@@ -1,32 +1,32 @@
 export const IMG_ROUTES = {
-    imgBanner: '/satin-blue-ela-test/assets/images/Recurso 17.jpg',
+    imgBanner: '/Satin-Blue_ELA/assets/images/Recurso 17.jpg',
 
     section1: {
-        img: '/satin-blue-ela-test/assets/images/Recurso 18.jpg',
+        img: '/Satin-Blue_ELA/assets/images/Recurso 18.jpg',
     },
     slide1: {
-        img1: '/satin-blue-ela-test/assets/images/SLIDE 1/Recurso 27.jpg',
-        img2: '/satin-blue-ela-test/assets/images/SLIDE 1/Recurso 28.jpg',
+        img1: '/Satin-Blue_ELA/assets/images/SLIDE 1/Recurso 27.jpg',
+        img2: '/Satin-Blue_ELA/assets/images/SLIDE 1/Recurso 28.jpg',
     },
 
     collection1: {
-        img1: '/satin-blue-ela-test/assets/images/Recurso 20.jpg',
-        img2: '/satin-blue-ela-test/assets/images/Recurso 21.jpg',
-        img3: '/satin-blue-ela-test/assets/images/Recurso 22.jpg',
-        img4: '/satin-blue-ela-test/assets/images/Recurso 23.jpg',
+        img1: '/Satin-Blue_ELA/assets/images/Recurso 20.jpg',
+        img2: '/Satin-Blue_ELA/assets/images/Recurso 21.jpg',
+        img3: '/Satin-Blue_ELA/assets/images/Recurso 22.jpg',
+        img4: '/Satin-Blue_ELA/assets/images/Recurso 23.jpg',
     },
 
     section2: {
-        img1: '/satin-blue-ela-test/assets/images/Recurso 29.jpg',
+        img1: '/Satin-Blue_ELA/assets/images/Recurso 29.jpg',
     },
     slide2: {
-        img1: '/satin-blue-ela-test/assets/images/Recurso 24.jpg',
-        img2: '/satin-blue-ela-test/assets/images/Recurso 25.jpg',
-        img3: '/satin-blue-ela-test/assets/images/Recurso 26.jpg',
+        img1: '/Satin-Blue_ELA/assets/images/Recurso 24.jpg',
+        img2: '/Satin-Blue_ELA/assets/images/Recurso 25.jpg',
+        img3: '/Satin-Blue_ELA/assets/images/Recurso 26.jpg',
     },
 
     collection2: {
-        img1: '/satin-blue-ela-test/assets/images/Recurso 31.jpg',
-        img2: '/satin-blue-ela-test/assets/images/Recurso 32.jpg',
+        img1: '/Satin-Blue_ELA/assets/images/Recurso 31.jpg',
+        img2: '/Satin-Blue_ELA/assets/images/Recurso 32.jpg',
     },
 };
